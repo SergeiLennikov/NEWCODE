ccf9c5a9e1f38a78b70d8a54f2f2b89568828b72
Sergei Lennikov <lennikov99@list.ru>
2024-03-16 22:59:52 +0700

    Массивы

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// int[] arr = new int[5];
+// arr[0] = 5;
+// arr[1] = 1;
+// arr[2] = 4;
+// arr[3] = 7;
+// arr[4] = 8;
+// System.Console.WriteLine(arr[5]);
+
+
+// int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+// System.Console.WriteLine(arr2[2]);
+
+arr3 = [1, 2, 3, 4, 5];
+System.Console.WriteLine(arr3[0]);
